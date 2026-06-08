3f8647bababf4fc3f79e91ec0fc42da0ddef6db5
Daniel Felix <felixdaniel-developer@outlook.com>
2026-06-08 14:05:45 -0300

    feat: implementado TerminalController para orquestracao da aplicacao.

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,19 @@
 import { PokeApiService } from "./services/PokeApiService";
 import { CatalogoPokemon } from "./services/CatalogoPokemon";
 import { BoxService } from "./services/BoxService";
+import { TerminalController } from "./controllers/TerminalController";
 
 async function main() {
   const api = new PokeApiService();
-
   const boxService = new BoxService();
 
   const catalogo = new CatalogoPokemon(boxService);
 
   await catalogo.carregar();
 
-  const pikachu = await api.buscarPokemon("pikachu");
+  const controller = new TerminalController(api, catalogo);
 
-  if (pikachu) {
-    await catalogo.adicionar(pikachu);
-  }
-
-  const charmander = await api.buscarPokemon("charmander");
-
-  if (charmander) {
-    await catalogo.adicionar(charmander);
-  }
-
-  const duplicado = await api.buscarPokemon("pikachu");
-
-  if (duplicado) {
-    await catalogo.adicionar(duplicado);
-  }
-
-  await api.buscarPokemon("pokemon-inexistente");
-
-  catalogo.listar();
-
-  await catalogo.remover(25);
-
-  catalogo.listar();
+  await controller.executar();
 }
 
 main();
